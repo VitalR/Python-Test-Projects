a43e1e1031c2af2d2e89b59a32ce881cd5990965
VitalR <vitalii.rodikov@gmail.com>
2020-06-23 21:01:01 +0300

    Second commit

---
+++ pages/home/home_page.py
@@ -0,0 +1,44 @@
+import logging
+import utilities.custom_logger as cl
+from selenium.webdriver.common.by import By
+from base.basepage import BasePage
+from pages.home.navigation_page import NavigationPage
+
+
+class HomePage(BasePage):
+    log = cl.custom_logger(logging.DEBUG)
+
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.driver = driver
+
+    # Locators
+    _login_link = 'Login'
+    _practice = 'Practice'
+    _header_logo = '.navbar-brand'  # css
+    _headline = 'headline'
+    _subtitle = 'subtitle'
+    _featured_courses = "//h2[contains(text(),'Featured Courses')]"
+    _primary_button = '.btn.btn-md.btn-primary'
+
+    # Actions
+    def verify_login_link_is_visible(self):
+        return self.is_element_present(self._login_link, locator_type='link')
+
+    def verify_practice_link_is_visible(self):
+        return self.is_element_present(self._practice, locator_type='link')
+
+    def verify_header_logo_is_visible(self):
+        return self.is_element_present(self._header_logo, locator_type='css')
+
+    def verify_headline_is_visible(self):
+        return self.is_element_present(self._headline, locator_type='class')
+
+    def verify_sibtitle_is_visible(self):
+        return self.is_element_present(self._subtitle, locator_type='class')
+
+    def verify_featured_courses_is_visible(self):
+        return self.is_element_present(self._featured_courses, locator_type='xpath')
+
+    def verify_primary_button_is_visible(self):
+        return self.is_element_present(self._primary_button, locator_type='css')
---
+++ tests/home/home_page_tests.py
@@ -0,0 +1,51 @@
+import pytest
+import unittest
+from pages.home.home_page import HomePage
+from utilities.teststatus import TestStatus
+from pages.home.login_page import LoginPage
+
+
+@pytest.mark.usefixtures('oneTimeSetUp', 'setUp')
+class HomePageTests(unittest.TestCase):
+
+    @pytest.fixture(autouse=True)
+    def classSetup(self, oneTimeSetUp):
+        self.hp = HomePage(self.driver)
+        self.ts = TestStatus(self.driver)
+        self.lp = LoginPage(self.driver)
+        self.lp.logout()
+
+    @pytest.mark.run(order=1)
+    def test_home_page_elements_01(self):
+        result = self.hp.verify_login_link_is_visible()
+        assert result == True
+
+    @pytest.mark.run(order=2)
+    def test_home_page_elements_02(self):
+        result = self.hp.verify_practice_link_is_visible()
+        assert result == True
+
+    @pytest.mark.run(order=3)
+    def test_home_page_elements_03(self):
+        result = self.hp.verify_header_logo_is_visible()
+        assert result == True
+
+    @pytest.mark.run(order=4)
+    def test_home_page_elements_04(self):
+        result = self.hp.verify_headline_is_visible()
+        assert result == True
+
+    @pytest.mark.run(order=5)
+    def test_home_page_elements_05(self):
+        result = self.hp.verify_sibtitle_is_visible()
+        assert result == True
+
+    @pytest.mark.run(order=6)
+    def test_home_page_elements_06(self):
+        result = self.hp.verify_featured_courses_is_visible()
+        assert result == True
+
+    @pytest.mark.run(order=7)
+    def test_home_page_elements_07(self):
+        result = self.hp.verify_primary_button_is_visible()
+        assert result == True
